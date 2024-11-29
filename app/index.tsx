eeac4f728af1de4630a77a45ccc7e437d6440616
C1322597 Giovana Covolo Fonseca <c1322597@interno.bb.com.br>
2024-11-29 20:37:02 -0300

    fix: validation fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,7 @@
 // app/index.tsx
 import React from "react";
 import HomeScreen from "./pages/home";
-import ManagePosts from "./pages/managePosts";
-import ManageUsers from "./pages/manageTeacher";
-import ManageStudent from "./pages/manageStudent";
 
 export default function Home() {
-  return <ManageStudent></ManageStudent>;
-  // return <ManageUsers></ManageUsers>;
-  // return <HomeScreen></HomeScreen>;
+  return <HomeScreen></HomeScreen>;
 }
